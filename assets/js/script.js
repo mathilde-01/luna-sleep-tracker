f13bdc321220d719229fc439b66726a34a4ecc03
Opal sn <59205540+OpalSnellneberger@users.noreply.github.com>
2023-11-02 19:05:35 -0400

    added conditions to submiting

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,26 +37,35 @@ if (element) {
     function submitForm(formId) {
         const eventType = document.getElementById('event-type').value;
         const message = document.getElementById('message').value;
-        const eventTime = document.getElementById('appt-time').value;
+        const beginEventTime = document.getElementById('begin-time').value;
+        const endEventTime = document.getElementById('end-time').value;
         const sleepHours = parseInt(document.getElementById('sleep-hours').value)
-
+        if (eventType.value === "Select") {
+            alert("Please select an option before submitting.");
+            formId.preventDefault(); // Prevent the form from being submitted.
+        } else if (beginEventTime   === "") {
+            alert("Please select a start time before submitting.");
+            formId.preventDefault();
+        } else if (endEventTime   === "") {
+            alert("Please select a end time before submitting.");
+            formId.preventDefault();
+        } else {
+            closeForm(formId);
+        }
         /* Create new div element to display submitted info in column */
 
         /* Conditional for adding text content */
 
         /* Get the corresponding day and append info */
 
-
         /* Conditional statement to alert each input */
-        if(formId === 'schedule-form'){
-            alert(`${eventType} event added: ${message} at ${eventTime}`);
-        }else if(formId === 'sleep-form' && sleepHours >= 8){
-            alert(`You slept for ${sleepHours} hours this day! I'm sure that is plenty.`)
-        }else if(formId === 'sleep-form' && sleepHours < 8 && sleepHours > 1){
-            alert(`You slept for ${sleepHours} hours this day! Binge watching Netflix again?`)
-        }else if(formId === 'sleep-form' && sleepHours < 2){
-            alert(`You slept for ${sleepHours} hour this day... Not great.`)
-        }
-
-        closeForm(formId);
+            if(formId === 'schedule-form'){
+                alert(`${eventType} event added: ${message} from ${beginEventTime} to ${endEventTime}`);
+            }else if(formId === 'sleep-form' && sleepHours >= 8){
+                alert(`You slept for ${sleepHours} hours this day! I'm sure that is plenty.`)
+            }else if(formId === 'sleep-form' && sleepHours < 8 && sleepHours > 1){
+                alert(`You slept for ${sleepHours} hours this day! Binge watching Netflix again?`)
+            }else if(formId === 'sleep-form' && sleepHours < 2){
+                alert(`You slept for ${sleepHours} hour this day... Not great.`)
+            }
     }
