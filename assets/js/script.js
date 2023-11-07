306058543805bdcdcc8473ead6957b035993652d
mathilde-01 <mathilde.7892@gmail.com>
2023-11-06 19:03:31 -0500

    added highlighted days

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // Bulma calendar
 // Initialize all input of type date
-var calendars = bulmaCalendar.attach('[type="date"]', { datePicker: "inline" });
+var calendars = bulmaCalendar.attach('[type="date"]', {
+  datePicker: "inline",
+  startDate: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 2),
+  endDate: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 3),
+});
 
 const moonImages = {
   firstQuarter: "assets/images/moon-icons/first-quarter.png",
@@ -38,16 +42,6 @@ if (element) {
   // bulmaCalendar instance is available as element.bulmaCalendar
   element.bulmaCalendar.on("select", function (datepicker) {
     console.log(datepicker.data.value());
-
-    // Get the selected date
-    const selectedDate = datepicker.data.value();
-
-    // Calculate 3 days before the selected date
-    const twoDaysBefore = new Date(selectedDate);
-    twoDaysBefore.setDate(twoDaysBefore.getDate() - 3);
-
-    // Set the date range in the input field
-    datepicker.data.range([twoDaysBefore, selectedDate]);
   });
 }
 
@@ -63,66 +57,81 @@ function closeForm(formId) {
   formPopup.classList.remove("is-active");
 }
 /* Submit form- takes in the responses and Alerts user of added event*/
-    function submitForm(formId) {
-        const formPopup = document.getElementById(formId);
-        const eventType = document.getElementById('event-type').value;
-        const message = document.getElementById('message').value;
-        const beginEventTime = document.getElementById('begin-time').value;
-        const endEventTime = document.getElementById('end-time').value;
-        const dayOffset = parseInt(formPopup.getAttribute('data-day-offset'))
-        const sleepHours = parseInt(document.getElementById('sleep-hours').value)
-        if (eventType.value === "Select") {
-            alert("Please select an option before submitting.");
-            formId.preventDefault(); // Prevent the form from being submitted.
-        } else if (beginEventTime   === "") {
-            alert("Please select a start time before submitting.");
-            formId.preventDefault();
-        } else if (endEventTime   === "") {
-            alert("Please select a end time before submitting.");
-            formId.preventDefault();
-        } else {
-            closeForm(formId);
-        }
+function submitForm(formId) {
+  const formPopup = document.getElementById(formId);
+  const eventType = document.getElementById("event-type").value;
+  const message = document.getElementById("message").value;
+  const beginEventTime = document.getElementById("begin-time").value;
+  const endEventTime = document.getElementById("end-time").value;
+  const dayOffset = parseInt(formPopup.getAttribute("data-day-offset"));
+  const sleepHours = parseInt(document.getElementById("sleep-hours").value);
+  if (eventType.value === "Select") {
+    alert("Please select an option before submitting.");
+    formId.preventDefault(); // Prevent the form from being submitted.
+  } else if (beginEventTime === "") {
+    alert("Please select a start time before submitting.");
+    formId.preventDefault();
+  } else if (endEventTime === "") {
+    alert("Please select a end time before submitting.");
+    formId.preventDefault();
+  } else {
+    closeForm(formId);
+  }
 
   /* Create new div element to display submitted info in column */
 
   /* Conditional for adding text content */
 
-        /* Get the corresponding day and append info */
-        const rawDay = dayjs().add(dayOffset, 'day');
-        const formatedDay = dayjs(rawDay).format('YYYY-MM-DD');
-        const formateBeginTime = dayjs(formatedDay + beginEventTime).format('YYYY-MM-DD HH:mm');
-        const formatedEndTime = dayjs(formatedDay + endEventTime).format('YYYY-MM-DD HH:mm');
-        const length =  dayjs(formatedEndTime).diff(dayjs(formateBeginTime), 'minute');
-        console.log(length);
-        
-        // [
-        //     {
-        //         "startHours": [],
-        //         "endHours": [],
-        //         "length": []
-        //     }
-        // ]
+  /* Get the corresponding day and append info */
+  const rawDay = dayjs().add(dayOffset, "day");
+  const formatedDay = dayjs(rawDay).format("YYYY-MM-DD");
+  const formateBeginTime = dayjs(formatedDay + beginEventTime).format(
+    "YYYY-MM-DD HH:mm"
+  );
+  const formatedEndTime = dayjs(formatedDay + endEventTime).format(
+    "YYYY-MM-DD HH:mm"
+  );
+  const length = dayjs(formatedEndTime).diff(dayjs(formateBeginTime), "minute");
+  console.log(length);
 
-        /* Conditional statement to alert each input */
-        if(formId === 'schedule-form'){
-            alert(`${eventType} event added: ${message} at ${eventTime}`);
-        }else if(formId === 'sleep-form' && sleepHours >= 8){
-            alert(`You slept for ${sleepHours} hours this day! I'm sure that is plenty.`)
-        }else if(formId === 'sleep-form' && sleepHours < 8 && sleepHours > 1){
-            alert(`You slept for ${sleepHours} hours this day! Binge watching Netflix again?`)
-        }else if(formId === 'sleep-form' && sleepHours < 2){
-            alert(`You slept for ${sleepHours} hour this day... Not great.`)
-        }
-    }
+  // [
+  //     {
+  //         "startHours": [],
+  //         "endHours": [],
+  //         "length": []
+  //     }
+  // ]
+
+  /* Conditional statement to alert each input */
+  if (formId === "schedule-form") {
+    alert(`${eventType} event added: ${message} at ${eventTime}`);
+  } else if (formId === "sleep-form" && sleepHours >= 8) {
+    alert(
+      `You slept for ${sleepHours} hours this day! I'm sure that is plenty.`
+    );
+  } else if (formId === "sleep-form" && sleepHours < 8 && sleepHours > 1) {
+    alert(
+      `You slept for ${sleepHours} hours this day! Binge watching Netflix again?`
+    );
+  } else if (formId === "sleep-form" && sleepHours < 2) {
+    alert(`You slept for ${sleepHours} hour this day... Not great.`);
+  }
+}
 
 // get the moon phase using city name and time
-function getMoonPhase(city, date){
+function getMoonPhase(city, date) {
+  var dateRangeMin = dayjs(date).subtract(3, "day").format("YYYY-MM-DD");
+  var dateRangeMax = dayjs(date).add(3, "day").format("YYYY-MM-DD");
 
-	var dateRangeMin = dayjs(date).subtract(3, "day").format("YYYY-MM-DD");
-	var dateRangeMax = dayjs(date).add(3, "day").format("YYYY-MM-DD");
-
-	fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + city + '/' + dateRangeMin + '/' + dateRangeMax + '?unitGroup=us&elements=moonphase&include=current&key=Z6C7FPXUVA9JACX4YZ6VGQPPK&contentType=json')
+  fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+      city +
+      "/" +
+      dateRangeMin +
+      "/" +
+      dateRangeMax +
+      "?unitGroup=us&elements=moonphase&include=current&key=Z6C7FPXUVA9JACX4YZ6VGQPPK&contentType=json"
+  )
     .then(function (response) {
       return response.json();
     })
@@ -186,30 +195,25 @@ function getCity() {
 
 getCity();
 
-
 /* Function to update day titles to selected day*/
 function updateDayTitles(selectedDate) {
-    const dayTitlesContainer = document.getElementById("day-titles-container");
+  const dayTitlesContainer = document.getElementById("day-titles-container");
 
-    if (dayTitlesContainer) {
-        // Clear the existing day titles
-        dayTitlesContainer.innerHTML = "";
+  if (dayTitlesContainer) {
+    // Clear the existing day titles
+    dayTitlesContainer.innerHTML = "";
 
-        // Calculate and set the day titles
-        for (let i = -3; i <= 3; i++) {
-            const day = selectedDate.add(i, 'day');
-            const dayOfWeek = day.format("dddd");
-            const date = day.format('MM/DD');
-            const column = document.createElement("div");
-            column.classList.add("column");
-            column.innerHTML = `<h2 class="title">${dayOfWeek} ${date}</h2>`;
-            dayTitlesContainer.appendChild(column);
-        }
+    // Calculate and set the day titles
+    for (let i = -3; i <= 3; i++) {
+      const day = selectedDate.add(i, "day");
+      const dayOfWeek = day.format("dddd");
+      const date = day.format("MM/DD");
+      const column = document.createElement("div");
+      column.classList.add("column");
+      column.innerHTML = `<h2 class="title">${dayOfWeek} ${date}</h2>`;
+      dayTitlesContainer.appendChild(column);
     }
+  }
 }
 // Initial update of day titles with the current date
 updateDayTitles(dayjs());
-
-
-
-
